eab6656bc55c5fab35481a39393379553569fd9f
Fadillah Akbar <anaksehat2081@gmail.com>
2025-07-09 10:50:37 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function showDetail(animeName) {
     alert("Kamu memilih: " + animename + "✨");
 }
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark');
+}
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // ganti setiap 4 detik
+}
